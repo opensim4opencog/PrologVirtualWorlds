9b7a83d9523c04226b2ea9ea50c6b37c2284cbdd
jwl <jwl@333c8890-0ee7-4917-8c0b-6467d23d71ca>
2001-12-09 15:57:13 +0000

    add the wrapper

--- trunk/wrappers/w.java
+++ trunk/wrappers/w.java
@@ -4,6 +4,15 @@ public class w
 {
   public static void main (String[] args) 
   {
+    // f is the "letter"; below, we put it in an "envelope"
     FileWriter f = new FileWriter("foo.txt");
-  } // end of main ()
+
+    // we can only do limited things to f: 4-5 forms of write(),
+    // a flush() and a close()... and that's it
+
+    // make the "envelope", put the "letter" in it
+    PrintWriter p = new PrintWriter(f);
+    // still, can do only limited things to f... but p has println()
+    // and will forward that to f in terms of f.write()
+  } // end of main (String[] args)
 }
