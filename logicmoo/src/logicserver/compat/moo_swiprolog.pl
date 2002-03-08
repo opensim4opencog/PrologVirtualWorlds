725394691c5d03d6f26cb51730c9bbbd75323eb7
Douglas R. Miles <logicmoo@gmail.com>
2002-03-08 14:39:53 +0000

    Each Prolog has there own way of loading files and declaring predicates.. the file sigma_bootstrap is supposed to hold the files to be loaded and the predicates dynamic.,.. the prolog specific implementations must first read this file and process accordingly.. (*oh one other slight change is i am going througgh the code and removing globally asserted default optioons)

--- logicmoo/src/logicserver/compat/moo_swiprolog.pl
+++ logicmoo/src/logicserver/compat/moo_swiprolog.pl
@@ -4,74 +4,31 @@
 % So first is loads the proper files and then starts up the system
 % ===================================================================
 
+
+% ===================================================================
+% Prolog Dependant Code
+% ===================================================================
+
+
 :-use_module(library(system)).
 :-use_module(library(shlib)).
 :-use_module(library(listing)).
 :-use_module(library(sgml)).
 :-use_module(library(rdf)).
-:- use_module(library(socket)).  
- %Load the TCP Library
-%:- use_module(library(unix)).      
-:- use_module(library(shell)).      
-:- use_module(library(shlib)).        
-:- use_module(library(url)).        
-:- use_module(library(quintus)).        
-:- use_module(library(qsave)).        
-    
-
-%:- use_module(library(backcomp)).        
+:- use_module(library(socket)).
+%Load the TCP Library
+%:- use_module(library(unix)).
+:- use_module(library(shell)).
+:- use_module(library(shlib)).
+:- use_module(library(url)).
+:- use_module(library(quintus)).
+:- use_module(library(qsave)).
 
 
-:-make.
-	
 :- style_check(-singleton).
 :- style_check(-discontiguous).
 :- style_check(-atom).
 :- style_check(-string).
-
-:-include('moo_header.pl').
-
-recompile_all :- !.
-
-
-
-multi_module(X):-
-	dynamic(X),
-	multifile(X),
-	module_transparent(X).
-
-register_moo_file_type(Filename,Type) :-
-	(moo_file_type(Filename,Type) ->
-		true;
-		assertz(moo_file_type(Filename,Type))),!.
-
-writeModeSet(Mode):-
-	(unsetMooOption(client=_)),
-	(setMooOption(client=Mode)),!.
-
-:-ensure_loaded('moo_utility.pl').
-
-:-absolute_file_name(.,Local),assert('LOGIC_ENGINE_RT'(Local)),assert(library_directory(Local)).
-
-:-absolute_file_name('../..',X),assert('ROOT_RT'(X)).
-
-% numbervars/1 (just simpler numbervars.. will use a rand9ome start point so if a partially numbered getPrologVars wont get dup getPrologVars
-
-numbervars(X):-get_time(T),convert_time(T,A,B,C,D,E,F,G),!,numbervars(X,'$VAR',G,_).
-
-once(X,Y):-once(X),once(Y).
-
-:- dynamic mooCache/1.
-:- dynamic mooCache/6.
-:- dynamic mooCache/5.
-:- dynamic mooCache/9.
-	
-
-
-moduleEnsureLoaded(X):-
-	catch(ensure_loaded(X),_,(catch((atom_concat('mod/',X,Y),
-	ensure_loaded(Y)),_,format(user_error,';; file find error ~q ~q',[X,E])))).
-	
 
 
 :- set_prolog_flag(optimise,false).
@@ -86,6 +43,16 @@ moduleEnsureLoaded(X):-
 :-set_prolog_flag(unknown,error).
 
 
+
+% numbervars/1 (just simpler numbervars.. will use a rand9ome start point so if a partially numbered getPrologVars wont get dup getPrologVars
+
+numbervars(X):-get_time(T),convert_time(T,A,B,C,D,E,F,G),!,numbervars(X,'$VAR',G,_).
+
+once(X,Y):-once(X),once(Y).
+
+moduleEnsureLoaded(X):-
+        catch(ensure_loaded(X),_,(catch((atom_concat('mod/',X,Y),
+        ensure_loaded(Y)),_,format(user_error,';; file find error ~q ~q',[X,E])))).
 
 callIfPlatformWin32(G):-prolog_flag(windows,true),!,ignore(G).
 callIfPlatformWin32(G):-!.
@@ -106,7 +73,7 @@ isCharCodelist([]).  isCharCodelist([A|T]):-integer(A),A>9,A<128,isCharCodelist(
 
 % Using retractall/1 (in Moo code) since Eclipse prolog (another port for Moo)  chokes on retractall/1
 
-:- current_prolog_flag(arch,'i386-win32') -> 
+:- current_prolog_flag(arch,'i386-win32') ->
       ensure_loaded('moo_swiprolog_win32.pl') ; ensure_loaded('moo_swiprolog_unix.pl').
 
 % Each prolog has a specific way it could unnumber the result of a numbervars
@@ -123,7 +90,7 @@ if_prolog(_,_):-!.  % Dont run SWI Specificd or others
 
 % ========================================================================================
 % Some prologs have a printf() type predicate.. so I made up fmtString/writeFmt in the Moo code that calls the per-prolog mechaism
-% in SWI it's formzat/N and sformat/N  
+% in SWI it's formzat/N and sformat/N
 % ========================================================================================
 
 writeFmtFlushed(X,Y,Z):-catch((format(X,Y,Z),flush_output(X)),_,true).
@@ -135,29 +102,67 @@ writeFmt(X):-format(X,[]).
 fmtString(X,Y,Z):-sformat(X,Y,Z).
 fmtString(Y,Z):-sformat(Y,Z).
 
-% =============================================
-% Sets Moo Defaults (After intialization)
-% =============================================
+prologAtInitalization(V):-at_initialization(V),!,logOnFailureIgnore(V).
 
-:-assert(show_entails).
-:-assert(show_all_explainations).
-:-assert(debug_tests).
 
+% ===================================================================
+% Semi-Prolog Dependant Code
+% ===================================================================
+sigma_ua(X):-processRequest(X).
+
+
+% -------------------------------------------------------------------
+% Load the Moo header
+% -------------------------------------------------------------------
+:-include('moo_header.pl').
+
+% -------------------------------------------------------------------
+% Load the Moo Engine
+% -------------------------------------------------------------------
 :-ensure_loaded('moo_bootstrap.pl').
 
-% =============================================
-% Load the Moo header and Engine 
-% =============================================
+% dynamic predicates
+processBootstrap:-
+   dynamicPredicate(X),
+   dynamic(X),
+   multifile(X),
+   module_transparent(X),
+   fail.
 
-prologAtInitalization(V):-at_initialization(V),!,logOnFailureIgnore(V).
+% index predicates
+processBootstrap:-
+      indexPredicate(X),
+      index(X),
+      fail.
+
+
+
+% load files
+processBootstrap:-
+   moduleFile(_,Filename),
+   ensure_loaded(Filename),fail.
+
+processBootstrap:-!.
+
+:-processBootstrap.
+
+
+
+
+% -------------------------------------------------------------------
+% Set Defaults
+% -------------------------------------------------------------------
 
 :-setMooOptionDefaults.
 
+% -------------------------------------------------------------------
+% Start the system
+% -------------------------------------------------------------------
 
-:-callIfPlatformUnix(prologAtInitalization(cs)).	 	
+:-callIfPlatformUnix(prologAtInitalization(cs)).
 
-%:-callIfPlatformUnix((prologAtInitalization(['mod/nani.sig']),prologAtInitalization(['mod/eliza.sig']),prologAtInitalization(kbot),prologAtInitalization(kbot2))).	 	
-
+:-callIfPlatformUnix((prologAtInitalization(['mod/mod_nani.pl']),prologAtInitalization(['mod/mod_eliza.pl']),
+         prologAtInitalization(kbot),prologAtInitalization(kbot2))).
 
 
 
--- logicmoo/src/logicserver/moo_planning.pl
+++ logicmoo/src/logicserver/moo_planning.pl
@@ -1,78 +1,74 @@
 /*
 
 Re-Implementaion of the WARPLAN System (Dr. Warren 1974)
-	Douglas Miles 1998
+        Douglas Miles 1998
 
 */
-
-
-
-
 
 
 % make_script(Start,Goal,Scripts)
 
 make_scripts(_,CurrentPropValues,impossible):-
-	unless(consistent_to(CurrentPropValues,true)), !.
-        
+        unless(consistent_to(CurrentPropValues,true)), !.
+
 make_scripts(Scripted,CurrentPropValues,ScriptMore):-
-	good_script(CurrentPropValues,true,Scripted,ScriptMore).
+        good_script(CurrentPropValues,true,Scripted,ScriptMore).
 
 %//------------------------------------------------------------------------------
 
-make_scripts(CurrentPropValues,_) :- 
-	unless(consistent_to(CurrentPropValues,true)), !,
-	write('impossible').
+make_scripts(CurrentPropValues,_) :-
+        unless(consistent_to(CurrentPropValues,true)), !,
+        write('impossible').
 
 make_scripts(CurrentPropValues,Scripted) :-
-	good_script(CurrentPropValues,true,Scripted,ScriptMore),
-	write_debug(make_scripts,ScriptMore).
+        good_script(CurrentPropValues,true,Scripted,ScriptMore),
+        write_debug(make_scripts,ScriptMore).
 
 %//------------------------------------------------------------------------------
 
 good_script(PropValues & CurrentPropValues,NextGoalPropValues,Scripted,ScriptX) :- !,
-	script_solves(PropValues,NextGoalPropValues,Scripted,GoalPropValuesFuture,ScriptMore),
-	good_script(CurrentPropValues,GoalPropValuesFuture,ScriptMore,ScriptX).
+        script_solves(PropValues,NextGoalPropValues,Scripted,GoalPropValuesFuture,ScriptMore),
+        good_script(CurrentPropValues,GoalPropValuesFuture,ScriptMore,ScriptX).
 
 good_script(PropValues,NextGoalPropValues,Scripted,ScriptMore) :-
-	script_solves(PropValues,NextGoalPropValues,Scripted,_,ScriptMore).
+        script_solves(PropValues,NextGoalPropValues,Scripted,_,ScriptMore).
 
 %//------------------------------------------------------------------------------
 
-script_solves(PropValues,NextGoalPropValues,Scripted,NextGoalPropValues,Scripted) :- 
-	readonly_state(PropValues).
+script_solves(PropValues,NextGoalPropValues,Scripted,NextGoalPropValues,Scripted) :-
+        readonly_state(PropValues).
 
 script_solves(PropValues,NextGoalPropValues,Scripted,GoalPropValuesFuture,Scripted) :-
-	does_achieve(PropValues,Scripted),
-	and(PropValues,NextGoalPropValues,GoalPropValuesFuture).
+        does_achieve(PropValues,Scripted),
+        and(PropValues,NextGoalPropValues,GoalPropValuesFuture).
 
-script_solves(PropValues,NextGoalPropValues,Scripted,PropValues & NextGoalPropValues,ScriptMore) :- 
-	enable_state_with_method(PropValues,Proposed_Method),
-	good_method(PropValues,Proposed_Method,NextGoalPropValues,Scripted,ScriptMore).
+script_solves(PropValues,NextGoalPropValues,Scripted,PropValues & NextGoalPropValues,ScriptMore) :-
+        enable_state_with_method(PropValues,Proposed_Method),
+        good_method(PropValues,Proposed_Method,NextGoalPropValues,Scripted,ScriptMore).
 
 %//------------------------------------------------------------------------------
 
-good_method(_,Proposed_Method,NextGoalPropValues,Scripted,ScriptMore:Proposed_Method ) :- 
-	preserves(Proposed_Method,NextGoalPropValues),
-	enabled_method_from_state(Proposed_Method,CurrentPropValues),
-	consistent_to(CurrentPropValues,NextGoalPropValues),
-	good_script(CurrentPropValues,NextGoalPropValues,Scripted,ScriptMore),
-	preserves(Proposed_Method,NextGoalPropValues).
+good_method(_,Proposed_Method,NextGoalPropValues,Scripted,ScriptMore:Proposed_Method ) :-
+        preserves(Proposed_Method,NextGoalPropValues),
+        enabled_method_from_state(Proposed_Method,CurrentPropValues),
+        consistent_to(CurrentPropValues,NextGoalPropValues),
+        good_script(CurrentPropValues,NextGoalPropValues,Scripted,ScriptMore),
+        preserves(Proposed_Method,NextGoalPropValues).
 
-good_method(PropValues,Proposed_Method,NextGoalPropValues,Scripted:Method,ScriptMore:Method) :- 
-	is_preserved_durring(PropValues,Method),
-	rollback(NextGoalPropValues,Method,GoalPropValuesFuture),
-	good_method(PropValues,Proposed_Method,GoalPropValuesFuture,Scripted,ScriptMore),
-	is_preserved_durring(PropValues,Method).
+good_method(PropValues,Proposed_Method,NextGoalPropValues,Scripted:Method,ScriptMore:Method) :-
+        is_preserved_durring(PropValues,Method),
+        rollback(NextGoalPropValues,Method,GoalPropValuesFuture),
+        good_method(PropValues,Proposed_Method,GoalPropValuesFuture,Scripted,ScriptMore),
+        is_preserved_durring(PropValues,Method).
 
 %//------------------------------------------------------------------------------
 
 does_achieve(PropValues,_:Method) :- enable_state_with_method(PropValues,Method).
 
-does_achieve(PropValues,Scripted:Method) :- !, 
-	is_preserved_durring(PropValues,Method),
-	does_achieve(PropValues,Scripted),
-	is_still_preserved_durring( PropValues, Method ).
+does_achieve(PropValues,Scripted:Method) :- !,
+        is_preserved_durring(PropValues,Method),
+        does_achieve(PropValues,Scripted),
+        is_still_preserved_durring( PropValues, Method ).
 
 does_achieve(PropValues,Scripted) :- initial_given_state(Scripted,PropValues).
 
@@ -82,67 +78,67 @@ is_still_preserved_durring(PropValues,Method) :- is_preserved_durring(PropValues
 
 %//------------------------------------------------------------------------------
 
-is_preserved_durring(PropValues,Method) :- 
-	imagine(PropValues & Method,0,_),
-	disabled_state_with_method(PropValues,Method), !, fail.
+is_preserved_durring(PropValues,Method) :-
+        imagine(PropValues & Method,0,_),
+        disabled_state_with_method(PropValues,Method), !, fail.
 
 is_preserved_durring(_,_).
 
 %//------------------------------------------------------------------------------
 
-preserves(Proposed_Method,PropValues & CurrentPropValues) :- 
-	is_preserved_durring(PropValues,Proposed_Method), 
-	preserves(Proposed_Method,CurrentPropValues).
+preserves(Proposed_Method,PropValues & CurrentPropValues) :-
+        is_preserved_durring(PropValues,Proposed_Method),
+        preserves(Proposed_Method,CurrentPropValues).
 
 preserves(_,true).
 
 %//------------------------------------------------------------------------------
 
-rollback(NextGoalPropValues,Method,GoalPropValues) :- 
-	enabled_method_from_state(Method,CurrentPropValues),
-	rollback1(NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture),
-	conjoin(CurrentPropValues,GoalPropValuesFuture,GoalPropValues).
-
-rollback1(PropValues & NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture) :- 
-	enable_state_with_method(OtherPropValues,Method),
-	equivalent(PropValues,OtherPropValues), !,
-	rollback1(NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture).
+rollback(NextGoalPropValues,Method,GoalPropValues) :-
+        enabled_method_from_state(Method,CurrentPropValues),
+        rollback1(NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture),
+        conjoin(CurrentPropValues,GoalPropValuesFuture,GoalPropValues).
 
 rollback1(PropValues & NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture) :-
-	element_of(OtherPropValues,CurrentPropValues),
-	equivalent(PropValues,OtherPropValues), !,
-	rollback1(NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture).
+        enable_state_with_method(OtherPropValues,Method),
+        equivalent(PropValues,OtherPropValues), !,
+        rollback1(NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture).
 
-rollback1(PropValues & NextGoalPropValues,Method,CurrentPropValues,PropValues & GoalPropValuesFuture) :- 
-	rollback1(NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture).
+rollback1(PropValues & NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture) :-
+        element_of(OtherPropValues,CurrentPropValues),
+        equivalent(PropValues,OtherPropValues), !,
+        rollback1(NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture).
+
+rollback1(PropValues & NextGoalPropValues,Method,CurrentPropValues,PropValues & GoalPropValuesFuture) :-
+        rollback1(NextGoalPropValues,Method,CurrentPropValues,GoalPropValuesFuture).
 
 rollback1(true,_,_,true).
 
 %//------------------------------------------------------------------------------
 
-consistent_to(CurrentPropValues,NextGoalPropValues) :- 
-	imagine(CurrentPropValues & NextGoalPropValues,0,_),
-	impossible(PropValues),
-	unless(unless(intersection_of(CurrentPropValues,PropValues))),
-	implied(PropValues,CurrentPropValues & NextGoalPropValues), 
-	!, fail.
+consistent_to(CurrentPropValues,NextGoalPropValues) :-
+        imagine(CurrentPropValues & NextGoalPropValues,0,_),
+        impossible(PropValues),
+        unless(unless(intersection_of(CurrentPropValues,PropValues))),
+        implied(PropValues,CurrentPropValues & NextGoalPropValues),
+        !, fail.
 
 consistent_to(_,_).
 
-% and                          
+% and
 
 %//------------------------------------------------------------------------------
 
-and(PropValues,NextGoalPropValues,NextGoalPropValues) :- 
-	element_of(OtherPropValues,NextGoalPropValues),
-	equivalent(PropValues,OtherPropValues), !.
+and(PropValues,NextGoalPropValues,NextGoalPropValues) :-
+        element_of(OtherPropValues,NextGoalPropValues),
+        equivalent(PropValues,OtherPropValues), !.
 
 and(PropValues,NextGoalPropValues,PropValues & NextGoalPropValues).
 
 %//------------------------------------------------------------------------------
 
 conjoin(PropValues & CurrentPropValues,NextGoalPropValues,PropValues & GoalPropValuesFuture) :- !,
-	conjoin(CurrentPropValues,NextGoalPropValues,GoalPropValuesFuture).
+        conjoin(CurrentPropValues,NextGoalPropValues,GoalPropValuesFuture).
 
 conjoin(PropValues,NextGoalPropValues,PropValues & NextGoalPropValues).
 
@@ -156,15 +152,15 @@ element_of(PropValues,PropValues).
 
 %//------------------------------------------------------------------------------
 
-intersection_of(LeftHandPropValues,RightHandPropValues) :- 
-	element_of(PropValues,LeftHandPropValues),
-	element_of(PropValues,RightHandPropValues).
+intersection_of(LeftHandPropValues,RightHandPropValues) :-
+        element_of(PropValues,LeftHandPropValues),
+        element_of(PropValues,RightHandPropValues).
 
 %//------------------------------------------------------------------------------
 
-implied(LeftHandPropValues & RightHandPropValues,CurrentPropValues) :- !, 
-	implied(LeftHandPropValues,CurrentPropValues),
-	implied(RightHandPropValues,CurrentPropValues).
+implied(LeftHandPropValues & RightHandPropValues,CurrentPropValues) :- !,
+        implied(LeftHandPropValues,CurrentPropValues),
+        implied(RightHandPropValues,CurrentPropValues).
 
 implied(PropValues,CurrentPropValues) :- element_of(PropValues,CurrentPropValues).
 
@@ -173,11 +169,11 @@ implied(PropValues,_) :- catch2(PropValues).
 implied(PropValues,_) :- instance_known(PropValues).
 
 %//------------------------------------------------------------------------------
-                                                                                                     
-notequal(PropValues,OtherPropValues) :- 
-	unless(PropValues=OtherPropValues),
-	unless(PropValues=imaginary(_)),
-	unless(OtherPropValues=imaginary(_)).
+
+notequal(PropValues,OtherPropValues) :-
+        unless(PropValues=OtherPropValues),
+        unless(PropValues=imaginary(_)),
+        unless(OtherPropValues=imaginary(_)).
 
 %//------------------------------------------------------------------------------
 
@@ -185,9 +181,9 @@ equivalent(PropValues,OtherPropValues) :- unless(nonequivalent(PropValues,OtherP
 
 %//------------------------------------------------------------------------------
 
-nonequivalent(PropValues,OtherPropValues) :- 
-	imagine(PropValues & OtherPropValues,0,_),
-	PropValues=OtherPropValues, !, fail.
+nonequivalent(PropValues,OtherPropValues) :-
+        imagine(PropValues & OtherPropValues,0,_),
+        PropValues=OtherPropValues, !, fail.
 
 nonequivalent(_,_).
 
