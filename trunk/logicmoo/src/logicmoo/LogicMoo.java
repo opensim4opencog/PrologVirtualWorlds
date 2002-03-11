5eb1b00455d3c14d90dcba015c2aa2bc767d163a
dmiles <dmiles@333c8890-0ee7-4917-8c0b-6467d23d71ca>
2002-03-11 02:04:44 +0000

    Added the two inreface Prolog uses to get references

--- trunk/logicmoo/src/logicmoo/LogicMoo.java
+++ trunk/logicmoo/src/logicmoo/LogicMoo.java
@@ -19,13 +19,23 @@ import com.jamud.creation.*;
 
 public class LogicMoo {
 
+		/*
+		Constructor called by prolog code:
+		
+		createJamud:-
+						 java_create_object('logicmoo.LogicMoo',JAMUD),
+						 format('\njamud.Jamud=~q\n',[JAMUD]),!,
+						 assert(jamud_object(JAMUD)),!.
+		
+		*/
+
 		public void LogicMoo() {
 				try {
-				LogfileWriter = new FileWriter(new File("/logicmoo.debug.txt"));
-				} catch (Exception e) {
+						LogfileWriter = new FileWriter(new File("/logicmoo.debug.txt"));
+				} catch ( Exception e ) {
 
 				}
-				
+
 				logWrite("Start jamud");
 				//startJamud();
 
@@ -38,15 +48,61 @@ public class LogicMoo {
 		public MudObjectRoot jamudMudObjectRoot; 
 		public MudObject jamudMudObjectBelowRoot; 
 
+		/* 
+		Temporary debugging for now
+		*/
 		public void logWrite(String msg) {
 				try {
 						System.out.println(msg);
-					   LogfileWriter.write(msg);
-				} catch (Exception e) {
+						LogfileWriter.write(msg);
+				} catch ( Exception e ) {
 
 				}
 		}
 
+		/*
+		
+		For loading references to Jamud basic objects as used in:
+
+		loadJamudReferences:-
+						  jamud_object(JAMUD),
+						  java_invoke_method(JAMUD,getJamudInstance(Instance)),
+						  java_invoke_method(JAMUD,getJamudMudObjectRoot(MudRoot)),!,
+										assert(jamud_instance(Instance)),
+										assert(jamud_root(MudRoot)).
+		
+		
+		*/              
+
+		public Jamud getJamudInstance() {
+				return  jamudInstance;
+		}
+
+		/*
+		
+		For loading references to Jamud basic objects as used in:
+
+		loadJamudReferences:-
+						  jamud_object(JAMUD),
+						  java_invoke_method(JAMUD,getJamudInstance(Instance)),
+						  java_invoke_method(JAMUD,getJamudMudObjectRoot(MudRoot)),!,
+										assert(jamud_instance(Instance)),
+										assert(jamud_root(MudRoot)).
+												
+		*/              
+
+		public MudObjectRoot getJamudMudObjectRoot() {
+				return  jamudMudObjectRoot;
+		}
+
+		/*
+						Boots the Jamud for now from prolog as:
+						
+						startJamud:-
+										 jamud_object(JAMUD),!,
+										 java_invoke_method(JAMUD,startJamud(X)).
+
+						*/              
 		public void startJamud() {
 				logWrite("Start jamud");
 				if ( jamudInstance==null ) {
@@ -55,8 +111,8 @@ public class LogicMoo {
 								Jamud.main( args );
 								jamudInstance = Jamud.currentInstance();
 								jamudMudObjectRoot = jamudInstance.mudObjectRoot();
-							 //   Iterator cobjs = jamudMudObjectRoot.childObjects();
-						 	 ///   jamudMudObjectBelowRoot = (MudObject)cobjs.next();
+								//   Iterator cobjs = jamudMudObjectRoot.childObjects();
+								///   jamudMudObjectBelowRoot = (MudObject)cobjs.next();
 						} catch ( Exception e ) {
 								logWrite("" +e );
 								e.printStackTrace(System.out);
