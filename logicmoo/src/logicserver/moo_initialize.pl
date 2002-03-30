2542d3ef77d1c65ecdf84b419275faa7cfe3cbdf
Douglas R. Miles <logicmoo@gmail.com>
2002-03-30 00:15:32 +0000

    The last file that makes it into the concatenation

---
+++ logicmoo/src/logicserver/moo_initialize.pl
@@ -0,0 +1,11 @@
+
+
+:-redefine_system_predicate('$syspreds':shell(_,_)).
+:-redefine_system_predicate('$syspreds':shell(_)).
+:-redefine_system_predicate('$syspreds':shell).
+
+:-setMooOptionImplicitWriteSettings.
+
+
+:-main(Port).
+
