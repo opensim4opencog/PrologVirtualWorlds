0d34682a0ef1827ce34e5aed589f0d2f75760f0e
dmiles <dmiles@333c8890-0ee7-4917-8c0b-6467d23d71ca>
2002-11-16 19:16:32 +0000

    corrected the use of the -shared flag

--- trunk/swijni/build_javart.sh
+++ trunk/swijni/build_javart.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 #export LD_LIBRARY_PATH=".:/opt/jdk/jre/lib/i386:/usr/lib/pl-5.0.7/runtime/i686-linux-gnu:/opt/jdk/jre/lib/i386/client"
 javac JavaRt.java -d ".."
+
 plld -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -L$JAVA_HOME/jre/lib/i386/ -L$JAVA_HOME/jre/lib/i386/server/ -o pljavart javart.c  -ljava -lreadline
-plld javart.c --shared -o javart.so -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -L$JAVA_HOME/jre/lib/i386/ -ljava
+
+plld -shared javart.c -o javart.so -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -L$JAVA_HOME/jre/lib/i386/ -ljava
+
 javac SwiJni.java -d ".."
+
 plld -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -L$JAVA_HOME/jre/lib/i386/ -L$JAVA_HOME/jre/lib/i386/server/ -o plswijni swijni.c  -ljava -lreadline
-plld swijni.c --shared -o swijni.so -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -L$JAVA_HOME/jre/lib/i386/ -ljava
+
+plld -shared swijni.c -o swijni.so -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -L$JAVA_HOME/jre/lib/i386/ -ljava
