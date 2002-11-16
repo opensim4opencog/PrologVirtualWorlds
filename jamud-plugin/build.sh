6f8c477ce8078a4313609efb82f86e8606a190a3
Douglas R. Miles <logicmoo@gmail.com>
2002-11-16 06:39:01 +0000

    Resopsitory Restructure

---
+++ jamud-plugin/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ../../bin/setpaths
+	
+# javac *.java
+
+# plld -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -L$JAVA_HOME/jre/lib/i386/ -o javart javart.c  -ljava 
+
+plld   javart.c  -shared -o javart.so -g -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/ -L$JAVA_HOME/jre/lib/i386/  -ljava 
+
+
+
+
